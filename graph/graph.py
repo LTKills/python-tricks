a073c4753c2367a1329c5cfe5da0aa56bbf9903d
Gabriel <gabs.oficial98@gmail.com>
2018-09-03 10:48:04 -0300

    Enhance input style

--- graph/graph.py
+++ graph/graph.py
@@ -28,7 +28,14 @@ class Graph:
         n_nodes = int(input('How many nodes? '))
 
         for index in range(n_nodes):
-            data = int(input('Value: '))
+            while(True):
+                try:
+                    data = int(input())
+                    break
+                except ValueError as error:
+                    print(str(error))
+                    continue
+
             node = Node(data, index)
             self.nodes.append(node)
 
@@ -37,8 +44,7 @@ class Graph:
         for _ in range(n_edges):
             while(True):
                 try:
-                    n1 = int(input())
-                    n2 = int(input())
+                    [n1, n2] = [int(a) for a in input().split()]
                     node1 = self.find_node(n1)
                     node2 = self.find_node(n2)
                     break
